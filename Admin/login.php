94f30e16ca175293976e52d116d98c4e7e719d8c
Deepak19196 <deep7rd@gmail.com>
2019-02-06 13:07:44 +0530

    'new'

--- Admin/login.php
+++ Admin/login.php
@@ -10,6 +10,33 @@
 
 </head>
 <body>
-
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 offset-4">
+         
+          <div class="card mt-5 bg-secondary border-primary shadow-lg" >
+ 
+  <div class="card-body">
+    <h3 class="card-title"> </h3>
+     <img class="card-img-top w-50 mx-auto d-block rounded-circle border shadow-md" src="../assets/images/manager.png" alt="Card image cap">
+   <hr>
+     <form action="" method="post">
+        <div class="form-group">
+          <input type="text" name="inputEmail" placeholder="Enter Your Email id" class="form-control">
+        </div>
+         <div class="form-group">
+          <input type="password" name="inputPassword" placeholder="Enter Your password" class="form-control">
+        </div>
+         <button type="submit" class="btn btn-indiatrade btn-block shadow-sm mb-5">Login</button>
+           <div class="alert alert-success alert-dismissible fade success_pop_up " role="alert">
+  <strong id="msg"></strong> 
+</div>
+     </form>
+  </div>
+  
+</div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
